b95e7b2918b6f723eaa80076ee15839c92b25e8c
nickscha <nick@scharnbeck.de>
2025-02-24 12:49:00 +0100

    [DOCS] More detailed usage example

--- examples/build.bat
+++ examples/build.bat
@@ -6,7 +6,7 @@ REM -ftime-report    /* To see compile/link time statistic */
 
 set DEF_COMPILER_FLAGS=-mconsole -march=native -mtune=native -std=c89 -pedantic -nodefaultlibs -nostdlib -mno-stack-arg-probe -Xlinker /STACK:0x100000,0x100000 ^
 -fno-builtin -ffreestanding -fno-asynchronous-unwind-tables -fuse-ld=lld -fsanitize=undefined -fsanitize-trap ^
--Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wmissing-field-initializers -Wuninitialized -Winit-self -Wunused -Wunused-macros -Wunused-local-typedefs
+-Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wmissing-field-initializers -Wno-uninitialized -Winit-self -Wunused -Wunused-macros -Wunused-local-typedefs
 
 set DEF_FLAGS_LINKER=-lkernel32
 
