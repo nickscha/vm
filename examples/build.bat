65e04cea9141ff3aeebb468649d077ecf39f9a7a
nickscha <nick@scharnbeck.de>
2025-04-02 13:48:01 +0200

    [BUID] remove sanitizer

--- examples/build.bat
+++ examples/build.bat
@@ -5,7 +5,7 @@ REM Compiles the program without the C standard library
 REM -ftime-report    /* To see compile/link time statistic */
 
 set DEF_COMPILER_FLAGS=-mconsole -march=native -mtune=native -std=c89 -pedantic -nodefaultlibs -nostdlib -mno-stack-arg-probe -Xlinker /STACK:0x100000,0x100000 ^
--fno-builtin -ffreestanding -fno-asynchronous-unwind-tables -fuse-ld=lld -fsanitize=undefined -fsanitize-trap ^
+-fno-builtin -ffreestanding -fno-asynchronous-unwind-tables -fuse-ld=lld ^
 -Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wmissing-field-initializers -Wno-uninitialized -Winit-self -Wunused -Wunused-macros -Wunused-local-typedefs
 
 set DEF_FLAGS_LINKER=-lkernel32
