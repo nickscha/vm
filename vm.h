ec446111db275fe38cf573a0c399f9f549b6d047
nickscha <nick@scharnbeck.de>
2025-08-28 17:32:32 +0200

    [VERSION] bump to v0.2

--- vm.h
+++ vm.h
@@ -1,4 +1,4 @@
-/* vm.h - v0.1 - public domain data structures - nickscha 2025
+/* vm.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) vector linear algebra implementation.
 
